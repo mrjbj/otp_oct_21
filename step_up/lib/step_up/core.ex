3f598825bbe90ebd0fb20aefd853c3fbaf564407
AbulAsar S <abul.asar22@gmail.com>
2021-10-20 21:33:26 +0530

    Add service/process api layer

--- step_up/lib/step_up/core.ex
+++ step_up/lib/step_up/core.ex
@@ -1,10 +1,10 @@
 defmodule StepUp.Core do
   def new(input), do: %{count: String.to_integer(input)}
 
-  def increment(map, step \\ 1), do: %{map | counter: map.counter + step}
+  def increment(map, step \\ 1), do: %{map | count: map.count + step}
 
   def decrement(map, step \\ 1), do: increment(map, -step)
-  
+
   def message(%{count: count}) do
     "the final value is #{count}"
   end
