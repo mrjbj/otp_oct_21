6b025bbd6d96d910783a913cb3e69e25bb8ede55
John Cotton <john@1080people.com>
2021-10-21 10:43:56 -0400

    refactor

--- amnesia/lib/amnesia/game/board.ex
+++ amnesia/lib/amnesia/game/board.ex
@@ -1,23 +1,18 @@
 defmodule Amnesia.Game.Board do
   # statuses: :playing, :won, :lost
-  defstruct answer: [], guesses: [], status: :playing
+  defstruct answer: [], guesses: []
 
-  def new(answer \\ [1, 2, 3, 4]) do
+  def new(answer \\ random()) do
     %__MODULE__{answer: answer}
-  end
-
-  def random do
-    1..8
-    |> Enum.shuffle()
-    |> Enum.take(4)
-    |> new()
   end
 
   def guess(board, current_guess) do
     %{board | guesses: [current_guess | board.guesses]}
   end
 
-  def as_map(%{status: :playing} = board) do
-    Map.from_struct(board)
+  defp random do
+    1..8
+    |> Enum.shuffle()
+    |> Enum.take(4)
   end
 end
