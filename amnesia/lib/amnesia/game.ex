9b24c98db0944ec23462097ec55d78c983223bca
AbulAsar S <abul.asar22@gmail.com>
2021-10-22 00:04:30 +0530

    add Supervisor for Game

--- amnesia/lib/amnesia/game.ex
+++ amnesia/lib/amnesia/game.ex
@@ -5,8 +5,8 @@ defmodule Amnesia.Game do
   #
   # Client API
   #
-  def start_link(answer) do
-    {:ok, _pid} = GenServer.start_link(__MODULE__, answer, name: __MODULE__)
+  def start_link(name) do
+    {:ok, _pid} = GenServer.start_link(__MODULE__, [], name: name)
   end
 
   def guess(pid \\ __MODULE__, numbers) do
@@ -26,5 +26,9 @@ defmodule Amnesia.Game do
   def handle_call({:guess, numbers}, _from, board) do
     new_board = Board.guess(board, numbers)
     {:reply, Board.as_map(new_board), new_board}
+  end
+
+  def child_spec(name) do
+    %{id: name, start: {__MODULE__, :start_link, [name]}}
   end
 end
